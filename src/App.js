053bbbdf8d6a5ddba6ff90db34e54ed4587ad64a
Mehedi <mehedialam109@gmail.com>
2021-11-04 22:10:47 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Appointment from './Pages/Appointment/Appointment/Appointment';
+import Home from './Pages/Home/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/appointment">
+            <Appointment />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import chair from '../../../images/chair.png';
+import bg from '../../../images/bg.png';
+import { Button, Typography, Container } from '@mui/material';
+
+const bannerBg = {
+  background: `url(${bg})`,
+  height: 450,
+};
+
+const verticalCenter = {
+  display: 'flex',
+  alignItems: 'center',
+  height: 400,
+};
+
+const Banner = () => {
+  return (
+    <div>
+      <Container style={bannerBg} sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid
+            item
+            style={{ ...verticalCenter, textAlign: 'left' }}
+            xs={12}
+            md={6}
+          >
+            <Box>
+              <Typography variant="h3">
+                Your New Smile <br />
+                Starts Here
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{ my: 3, fontSize: 13, fontWeight: 300, color: 'gray' }}
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo labore dolorem quam distinctio dicta praesentium enim
+                neque voluptate earum nihil!
+              </Typography>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: '#5CE7ED' }}
+              >
+                Get Appointment
+              </Button>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6} style={verticalCenter}>
+            <img style={{ width: 350 }} src={chair} alt="" />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
